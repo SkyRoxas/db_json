edce9d77c256beba7556b974af71363cbdc0ce2c
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-02 02:47:30 +0800

    json to obj

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-jQuery(document).ready(function(){
-  
+jQuery(document).ready(function() {
+    var json = '{"user_info":[{"title":"Roxas","documents":"a super handsome boy","uid": 1},{"title":"SkyRoxas","documents":"a super handsome boy","uid": 2}]}';
+    var obj = JSON.parse(json);
+    console.log(obj.user_info[1].title);
+    //$('#content').html(obj[0].title);
+
 })
