a696ce0034768ff520be93047ebd633e4bde0c04
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-03 01:59:13 +0800

    json each function

--- script.js
+++ script.js
@@ -5,13 +5,20 @@ jQuery(document).ready(function() {
 
 
     //keys length
-    for (var i = 0; i < Object.keys(obj.user_info).length; i++) {
-        //console.log(Object.keys(obj.user_info[i]).length);
-        for (var k = 0; k < Object.keys(obj.user_info[i]).length; k++) {
-            console.log(Object.keys(obj.user_info[i])[k]);
-        }
-    }
+    // for (var i = 0; i < Object.keys(obj.user_info).length; i++) {
+    //     //console.log(Object.keys(obj.user_info[i]).length);
+    //     for (var k = 0; k < Object.keys(obj.user_info[i]).length; k++) {
+    //         console.log(Object.keys(obj.user_info[i])[k]);
+    //     }
+    // }
 
+    $.each(obj, function(i, val){
+        if (i=="user_info"){
+            $.each(val[0] , function(applier, a_val){
+                alert("key:"+applier+" , value:"+a_val)
+            });
+        }
+    });
     //$('#content').html(obj[0].title);
 
 })
