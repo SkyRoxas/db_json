8b50ed9993b3756770018133af9479723da90736
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-03 01:36:17 +0800

    json for object key

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 jQuery(document).ready(function() {
     var json = '{"user_info":[{"title":"Roxas","documents":"a super handsome boy","uid": 1},{"title":"SkyRoxas","documents":"a super handsome boy","uid": 2}]}';
     var obj = JSON.parse(json);
-    console.log(obj.user_info[1].title);
+
+
+
+    //keys length
+    for (var i = 0; i < Object.keys(obj.user_info).length; i++) {
+        //console.log(Object.keys(obj.user_info[i]).length);
+        for (var k = 0; k < Object.keys(obj.user_info[i]).length; k++) {
+            console.log(Object.keys(obj.user_info[i])[k]);
+        }
+    }
+
     //$('#content').html(obj[0].title);
 
 })
