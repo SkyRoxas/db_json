46f675a6023754cd6154aa2ee991a603d9d5ed64
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-04 15:07:39 +0800

    file wrapper

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,15 @@ jQuery(document).ready(function() {
         //console.log("JSON Data: " + json);
         $.each(json, function($key, $val) {
           for (var i = 0; i < $val.length; i++) {
+            $('#content').append('<div class ="file item_'+i+'"></div>');
             if ($key == "user_info") {
               //console.log($val.length);
                 $.each($val[i], function($key, $val) {
                     console.log("key:" + $key + " , value:" + $val)
-                    $('#content').append('<div class ="field field-item'+i+'+"><div class="field-title">'+$key+':</div><div class ="field-content">'+$val+'</div>')
+                    $('#content').find('.item_'+i+'')
+                    .append(
+                      '<div class ="field field_'+$key+'"><div class="field-label field-item">'+$key+'</div><div class ="field-content field-item">'+$val+'</div>'
+                    )
                 });
             }
           }
