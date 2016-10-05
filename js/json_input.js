e50e259f7062725778d39b2e5ed5bc541ce0cc37
SkyRoxas <SkyRoxas1521@gmail.com>
2016-10-05 20:25:34 +0800

    print data

--- js/json_input.js
+++ js/json_input.js
@@ -1,20 +1,46 @@
 jQuery(document).ready(function($) {
     var obj = new Object;
+    var perfileArray = [];
+    var $pid = 0;
+    obj.perfile = perfileArray;
 
-    console.log(obj);
-    //console.log($('textarea').attr('name'));
 
     function btnClick() {
         var outputObj = new Object;
+
         $('.inputData').each(function() {
             var $name = $(this).attr('name');
             var $value = $(this).val();
             outputObj[$name] = $value;
-            console.log(outputObj);
         })
+
+        outputObj.pid = $pid += 1;
+        console.log(outputObj);
+
+        perfileArray.push(outputObj);
+        console.log(perfileArray);
+        console.log(JSON.stringify(obj));
+
+        $.each(obj, function($key, $val) {
+            //$('#content .JsonOutput').append('<div class ="file item_"></div>');
+            if ($key == "perfile") {
+                $.each($val[$val.length -1], function($key, $val) {
+                    $('#content .JsonOutput')
+                        .append(
+                            '<div class ="field field_' + $key + '">' +
+                            '<div class="field-label field-item">' + $key + '</div>' +
+                            '<div class ="field-content field-item">' + $val + '</div>' +
+                            '</div>'
+                        )
+                });
+
+            }
+
+        });
     }
 
     $('button').click(function() {
         btnClick();
     })
+
 })
